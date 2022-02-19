3ad1a24236300b95b6964bc26643e571a819e2a9
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-02-19 14:23:06 +0330

    new
    new

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const getData = async function(){
       const cards = document.querySelectorAll(".card")
       console.log(cards)
       for(let card of cards){
-        if (!card.textContent.toLowerCase().includes(Value)) {
+        if (!card.innerText.toLowerCase().includes(Value)) {
           card.style.display = "none";
         } else {
           card.style.display = "block";
